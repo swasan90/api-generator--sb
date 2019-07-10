ac5265081b3487a8bc11b052a4124e845e0fd0ba
unknown <swathyphpfreelancer@gmail.com>
2019-07-10 16:45:37 -0400

    added allow filtering annotation

--- src/main/java/com/springboot/apigenerator/repository/ProjectDomainRepository.java
+++ src/main/java/com/springboot/apigenerator/repository/ProjectDomainRepository.java
@@ -2,6 +2,7 @@ package com.springboot.apigenerator.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import com.springboot.apigenerator.model.ProjectDomain;
 
 @Repository
 public interface ProjectDomainRepository extends CassandraRepository<ProjectDomain,UUID>{
-
+	
+	@AllowFiltering
+	ProjectDomain findByProjectNameAndDomainName(String projectName,String domainName);
 }
