4567db8ee03f188f0f85c9912237bcd1c299421d
unknown <swathyphpfreelancer@gmail.com>
2019-07-11 16:55:30 -0400

    refactored the code

--- src/main/java/com/springboot/apigenerator/service/SchemaBuilderServiceImpl.java
+++ src/main/java/com/springboot/apigenerator/service/SchemaBuilderServiceImpl.java
@@ -1,16 +1,16 @@
+/**
+ * Class to implement create schema for the given domain.
+ */
 package com.springboot.apigenerator.service;
 
 import java.util.Optional;
-import java.util.StringJoiner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.stereotype.Service;
 
-import com.springboot.apigenerator.exceptions.EntityFoundException;
 import com.springboot.apigenerator.model.FieldWrapper;
 import com.springboot.apigenerator.model.ProjectDomain;
 import com.springboot.apigenerator.model.SchemaBuilder;
@@ -23,37 +23,49 @@ public class SchemaBuilderServiceImpl implements SchemaBuilderService {
 
 	@Autowired
 	private ProjectDomainRepository projectDomainRepo;
-	
+
 	@Autowired
 	private CassandraOperations cassandraTemplate;
-	 
 
+	/**
+	 * Function to implement create schema method.
+	 * @return boolean
+	 *   
+	 */
 	@Override
-	public void createSchemaForDomain(SchemaBuilder schema) throws EntityFoundException {
-
+	public boolean createSchemaForDomain(SchemaBuilder schema) {
 		Optional<ProjectDomain> project = projectDomainRepo.findById(schema.getProjectId());
-		try {
-			if (project.isPresent()) {	
-				logger.info("Constructing Query");
-				StringJoiner strJoin = new StringJoiner(",");					 
-				for (FieldWrapper field : schema.getFields()) {						
-					 StringBuilder strBuild = new StringBuilder();
-					 strBuild.append(field.getFieldName()).append(" ").append(findFieldType(field));		
-					 strJoin.add(strBuild);					 
-				}	
-				String cqlQuery = "CREATE TABLE "+project.get().getDomainName().toLowerCase()+
-						"( project_id UUID , id UUID,"+strJoin+",PRIMARY KEY(project_id,id))";
- 				cassandraTemplate.getCqlOperations().execute(cqlQuery);
- 				logger.info("created table");
+		if (project.isPresent()) {
+
+			logger.info("Constructing Query");
+			StringBuilder fields = new StringBuilder();
+
+			// Constructing field name and field type as single string using string builder.
+			for (FieldWrapper field : schema.getFields()) {
+				fields.append(field.getFieldName()).append(" ").append(findFieldType(field)).append(",");
 			}
 
-		} catch (DataIntegrityViolationException e) {
-			logger.error("Catching Exception " + e.getMessage());
-			throw new EntityFoundException(e.getMessage());
+			// Constructing cql query to create table.
+			StringBuilder cqlQuery = constructCqlQuery(project.get(), fields);
+
+			// Creating table using cassandra template.
+			cassandraTemplate.getCqlOperations().execute(cqlQuery.toString());
+
+			logger.info("created table");
+			return true;
+		} else {
+			logger.error("No project object exists with the project id " + project.get().getId());
+			return false;
 		}
 
 	}
 
+	/**
+	 * Function to return the cassandra data type for the given field type.
+	 * 
+	 * @param field
+	 * @return fieldType
+	 */
 	private String findFieldType(FieldWrapper field) {
 		String fieldType;
 		switch (field.getFieldType()) {
@@ -71,8 +83,20 @@ public class SchemaBuilderServiceImpl implements SchemaBuilderService {
 			break;
 
 		}
-
 		return fieldType;
+	}
 
+	/**
+	 * Function to construct cql query to create table.
+	 * 
+	 * @param project
+	 * @param fields
+	 * @return cqlQuery
+	 */
+	private StringBuilder constructCqlQuery(ProjectDomain project, StringBuilder fields) {
+		StringBuilder cqlQuery = new StringBuilder();
+		cqlQuery.append("CREATE TABLE ").append(project.getDomainName().toLowerCase())
+				.append("(project_id UUID, id UUID,").append(fields).append("PRIMARY KEY(project_id,id))");
+		return cqlQuery;
 	}
 }
