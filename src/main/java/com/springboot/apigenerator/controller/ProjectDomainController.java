8cf0fb17476238c9562138c07d0ac864bf37fe03
unknown <swathyphpfreelancer@gmail.com>
2019-07-11 17:00:57 -0400

    removed system .out

--- src/main/java/com/springboot/apigenerator/controller/ProjectDomainController.java
+++ src/main/java/com/springboot/apigenerator/controller/ProjectDomainController.java
@@ -30,8 +30,7 @@ public class ProjectDomainController {
 
 	
 	@PostMapping(value="/createProject",consumes="application/json",produces="application/json")
-	public ResponseEntity<ResponseMessage> createProject(@RequestBody ProjectDomain project) throws EntityFoundException{
-		System.out.println("printing projec values "+project.getProjectName()+" "+project.getDomainName());
+	public ResponseEntity<ResponseMessage> createProject(@RequestBody ProjectDomain project) throws EntityFoundException{		
 		if(projectService.createProjectDomain(project)) {
 			logger.info("Successfully created project");
 			return new ResponseEntity<ResponseMessage>(this.res.setMessage("Successfully created project",true), HttpStatus.CREATED);
