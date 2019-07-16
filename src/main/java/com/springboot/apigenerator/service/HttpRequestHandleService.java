3868557404c0f627d42dacf0994b09322bb2f59a
unknown <swathyphpfreelancer@gmail.com>
2019-07-16 14:52:29 -0400

    created http request handler service interface to define methods

---
+++ src/main/java/com/springboot/apigenerator/service/HttpRequestHandleService.java
@@ -0,0 +1,17 @@
+package com.springboot.apigenerator.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.springboot.apigenerator.model.RequestPayload;
+
+public interface HttpRequestHandleService  {
+	
+	<T> List<Map<String, Object>> listAll(String project_name,String domain_name);
+	
+	boolean insertRecord(RequestPayload payload,String projectName,String domainName) throws Exception;
+	
+	List<Map<String,Object>> getRecordByID(String project_name,String domain_name,UUID id);
+
+}
