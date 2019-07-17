2a6fa2f9e8ca72c67ce73e616ad0e37bacaed4c7
unknown <swathyphpfreelancer@gmail.com>
2019-07-16 22:37:39 -0400

    added @jsonignore to remove null values from response

--- src/main/java/com/springboot/apigenerator/model/ResponseMessage.java
+++ src/main/java/com/springboot/apigenerator/model/ResponseMessage.java
@@ -6,12 +6,15 @@ package com.springboot.apigenerator.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * @author swathy
  * @param <T>
  *
  */
-
+@JsonInclude(Include.NON_NULL)
 public class ResponseMessage {
 
 	private String message;
