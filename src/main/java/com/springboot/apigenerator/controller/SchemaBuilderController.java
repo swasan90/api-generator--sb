731f402779c4945b0ab20b5e8fae53029ac6283d
unknown <swathyphpfreelancer@gmail.com>
2019-07-24 16:33:24 -0400

    changed response message setter function signature

--- src/main/java/com/springboot/apigenerator/controller/SchemaBuilderController.java
+++ src/main/java/com/springboot/apigenerator/controller/SchemaBuilderController.java
@@ -48,12 +48,12 @@ public class SchemaBuilderController {
 	public ResponseEntity<ResponseMessage> buildSchema(@Valid @RequestBody SchemaGenerator schema) {
 		if(schemaBuilderService.createSchemaForDomain(schema)) {
 			logger.info("Successfully created table for the given domain ");
-			return new ResponseEntity<ResponseMessage>(this.res.setMessage("Successfully created table",true), HttpStatus.CREATED);
+			return new ResponseEntity<ResponseMessage>(this.res.setMessage("Successfully created table",true,null), HttpStatus.CREATED);
 			
 		}else {
 			logger.error("Unable to create table");
 			return new ResponseEntity<ResponseMessage>(
-					this.res.setMessage("Project/domain doesn't exists.Unable to create",false),
+					this.res.setMessage("Project/domain doesn't exists.Unable to create",false,null),
 					HttpStatus.BAD_REQUEST);
 		}
 	}
