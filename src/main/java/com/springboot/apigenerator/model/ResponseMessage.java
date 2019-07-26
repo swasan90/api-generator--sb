0627eed1a1ab4987051e450feaa317a8c10c65fe
unknown <swathyphpfreelancer@gmail.com>
2019-07-26 10:35:20 -0400

    removed unnecessary methods and implemented overloading on set data function

--- src/main/java/com/springboot/apigenerator/model/ResponseMessage.java
+++ src/main/java/com/springboot/apigenerator/model/ResponseMessage.java
@@ -5,6 +5,7 @@
 package com.springboot.apigenerator.model;
 
 import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -23,28 +24,34 @@ public class ResponseMessage {
 	
 	private List<?> data;
 	
+	private Set<?> dataSet; 
+
 	private Object resObj;
 
 	public ResponseMessage() {
-	}
-
-	public ResponseMessage setMessage(String message, boolean status,Object resObj) {
-		this.message = message;
-		this.status = status;		
-		this.resObj = resObj;
-		return this;
-	}
-
-	public ResponseMessage setStatus(boolean status) {
-		this.status = status;
-		return this;
 	}
 	
 	public ResponseMessage setData(List<?> data,boolean status) {		 
 		this.data = data;
 		this.status = status;
 		return this;
+	}	
+	
+	 
+	public ResponseMessage setData(Set<?> projects,boolean status) {		 
+		this.dataSet = projects;
+		this.status = status;		 
+		return this;
 	}
+	
+	public ResponseMessage setData(String message, boolean status,Object resObj) {
+		this.message = message;
+		this.status = status;		
+		this.resObj = resObj;
+		return this;
+	}
+	 
+	/**getters**/
 	
 	public List<?> getData() {
 		return data;
@@ -62,6 +69,26 @@ public class ResponseMessage {
 		return resObj;
 	}
 	
+	public Set<?> getDataSet() {
+		return dataSet;
+	}
+	
+	
+	
+//	
+//	public ResponseMessage setStatus(boolean status) {
+//		this.status = status;
+//		return this;
+//	}
+	
+//	public ResponseMessage setMessage(String message, boolean status,Object resObj) {
+//	this.message = message;
+//	this.status = status;		
+//	this.resObj = resObj;
+//	return this;
+//}
+ 
+//	
 	 
 
 }
