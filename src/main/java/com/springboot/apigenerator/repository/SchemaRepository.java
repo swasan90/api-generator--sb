34f0abe7f3829eb82c341ebaa6eff30d4b0917b5
unknown <swathyphpfreelancer@gmail.com>
2019-07-31 13:48:10 -0400

    added function to find by project uuid

--- src/main/java/com/springboot/apigenerator/repository/SchemaRepository.java
+++ src/main/java/com/springboot/apigenerator/repository/SchemaRepository.java
@@ -3,6 +3,7 @@
  */
 package com.springboot.apigenerator.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
@@ -16,5 +17,6 @@ import com.springboot.apigenerator.model.SchemaMapping;
  */
 @Repository
 public interface SchemaRepository extends CassandraRepository<SchemaMapping, UUID>{
-
+	
+	List<SchemaMapping> findByProjectId(UUID id);
 }
