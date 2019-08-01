b8bc86074a431abfe13d731b3970f195413b1289
unknown <swathyphpfreelancer@gmail.com>
2019-08-01 14:10:31 -0400

    changed incoming url port

--- src/main/java/com/springboot/apigenerator/config/SecurityConfig.java
+++ src/main/java/com/springboot/apigenerator/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);		 
-		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedOrigin("http://localhost:4210");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("OPTIONS");
 		config.addAllowedMethod("GET");
